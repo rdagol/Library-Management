71043cbcfc2761c0e3d7cbbf3f3086f5abb87719
NguyenDucTuan15 <145081082+TunaH98@users.noreply.github.com>
2024-03-18 21:45:24 +0700

    tuantest
    hihi

--- Lib_management/src/View/LoginForm.java
+++ Lib_management/src/View/LoginForm.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package View;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Tùng
- */
+
 public class LoginForm extends javax.swing.JFrame {
 
-    /**
-     * Creates new form LoginForm
-     */
+    
     public LoginForm() {
         initComponents();
         setLocationRelativeTo(null);
